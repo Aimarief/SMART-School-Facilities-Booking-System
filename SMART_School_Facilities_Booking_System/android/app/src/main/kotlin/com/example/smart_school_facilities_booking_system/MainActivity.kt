80dd2410d062fbad1253ee72216778bcb348b2c8
Aimarief <139568912+Aimarief@users.noreply.github.com>
2025-08-27 01:42:46 +0800

    Delete for big change
    Change including gradle and many files and folder

--- SMART_School_Facilities_Booking_System/android/app/src/main/kotlin/com/example/smart_school_facilities_booking_system/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.smart_school_facilities_booking_system
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- SMART_School_Facilities_Booking_System/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:smart_school_facilities_booking_system/main.dart';
-
-void main() {
-  testWidgets('App starts up without crashing', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-    expect(find.byType(MyApp), findsOneWidget);
-  });
-}
